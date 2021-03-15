3a21a66b187a1b79bc1a52f65ac784992a5ac704
Kibuto <nguyentrongnhanc8@gmail.com>
2021-03-15 21:47:41 +0700

    initial commit

---
+++ EAXamarinApp/EAXamarinApp.Android/MainActivity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+namespace EAXamarinApp.Droid
+{
+    [Activity(Label = "EAXamarinApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            AppCenter.Start("ad984717-0376-4147-9650-9264797d15b5",
+                   typeof(Analytics), typeof(Crashes));
+
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+        }
+    }
+}
